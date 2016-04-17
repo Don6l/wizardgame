6bf7bdfd8a84da9e8f7b2744a62f40db5bb27979
don6l <donalmcclean@gmail.com>
2016-04-17 22:24:47 +0100

    Original commit and push

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject PauseUI;
+	public GameObject playMusic;
+	public GameObject pauseMusic;
+	private bool paused = false;
+
+	void Start(){
+	
+		PauseUI.SetActive (false);
+
+	}
+
+	void Update(){
+		playMusic = GameObject.Find ("AudioManager");
+		AudioSource audioSourcePlay = playMusic.GetComponent<AudioSource> ();
+		pauseMusic = GameObject.Find ("PauseAudio");
+		AudioSource audioSourcePause = pauseMusic.GetComponent<AudioSource> ();
+		audioSourcePause.Pause ();
+		if (Input.GetButtonDown ("Pause")) {
+			paused = !paused;
+
+		}
+	
+		if (paused) {
+			audioSourcePlay.Pause ();
+			audioSourcePause.Play();
+			PauseUI.SetActive (true);
+			Time.timeScale = 0;
+
+		}
+
+		if (!paused) {
+			audioSourcePlay.UnPause ();
+			audioSourcePause.Stop();
+			PauseUI.SetActive (false);
+			Time.timeScale = 1;
+
+		}
+	}
+
+
+	public void Resume(){
+		paused = false;
+	}
+
+	public void Restart(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+
+	public void Exit(){
+		Application.Quit ();
+	}
+}
